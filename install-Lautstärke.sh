25693152b172848a2e155cdbe96539c9b0d3b252
FinnLippok <77006988+FinnPL@users.noreply.github.com>
2021-03-08 09:25:59 +0100

    added sudo apt-get install python-alsaaudio

--- install-Lautstärke.sh
+++ install-Lautstärke.sh
@@ -3,6 +3,7 @@ read REPLY
 if [[ ! "$REPLY" =~ ^(yes|y|Y)$ ]]; then exit 0; fi
 
 sudo apt install python3 idle3
+sudo apt-get install python-alsaaudio
 
 sudo apt-get install python-rpi.gpio python3-rpi.gpio
 
