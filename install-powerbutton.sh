c779f704afa90cd658185f2e4c0b8f018b2c61d9
FinnLippok <77006988+FinnPL@users.noreply.github.com>
2021-03-08 08:55:08 +0100

    Update install-powerbutton.sh

--- install-powerbutton.sh
+++ install-powerbutton.sh
@@ -1,2 +1,5 @@
+vecho -n "Willst du ein poweroff button einrichten? [y/N] "
+read REPLY
+if [[ ! "$REPLY" =~ ^(yes|y|Y)$ ]]; then exit 0; fi
 sudo mv poweroffbutton.py /usr/local/bin/
 sudo chmod +x /usr/local/bin/poweroffbutton.py
