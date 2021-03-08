594ddf705f819093e2872c626b141f47bc062a60
FinnLippok <77006988+FinnPL@users.noreply.github.com>
2021-03-08 10:08:10 +0100

    addedroot check

--- install-powerbutton.sh
+++ install-powerbutton.sh
@@ -1,3 +1,5 @@
+if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
+
 echo -n "Willst du ein poweroff button einrichten? [y/N] "
 read REPLY
 if [[ ! "$REPLY" =~ ^(yes|y|Y)$ ]]; then exit 0; fi
