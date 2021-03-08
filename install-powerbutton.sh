3f1fb3a43eee73ae80f53208c1c41289938e40d9
FinnLippok <77006988+FinnPL@users.noreply.github.com>
2021-03-08 09:06:23 +0100

    installing python

--- install-powerbutton.sh
+++ install-powerbutton.sh
@@ -1,6 +1,7 @@
 vecho -n "Willst du ein poweroff button einrichten? [y/N] "
 read REPLY
 if [[ ! "$REPLY" =~ ^(yes|y|Y)$ ]]; then exit 0; fi
+sudo apt install python3 idle3
 sudo mv poweroffbutton.py /usr/local/bin/
 sudo chmod +x /usr/local/bin/poweroffbutton.py
 
